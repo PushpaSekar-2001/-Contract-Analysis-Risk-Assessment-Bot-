8b05fad25f5b8b3cca49ed28ae303217c09fe022
Pushpa Sekar <128773599+PushpaSekar-2001@users.noreply.github.com>
2026-02-05 22:52:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ import json
 from datetime import datetime
 from backend.main import analyze_contract, get_summary_report
 import os
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 # Page configuration
 st.set_page_config(
